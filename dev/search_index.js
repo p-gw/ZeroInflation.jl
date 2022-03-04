befb08e48c289a576d1167269656728ce677eab7
Documenter.jl <documenter@juliadocs.github.io>
2022-03-04 16:42:06 +0000

    build based on 7b3ae7e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ZeroInflation","category":"page"},{"location":"#ZeroInflation","page":"Home","title":"ZeroInflation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ZeroInflation.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ZeroInflation]","category":"page"}]
+}
