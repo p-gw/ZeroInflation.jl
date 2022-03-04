7b3ae7ecb884585a8b37a6aa7091f3e964bdc5ea
Philipp Gewessler <p.gewessler@gmx.at>
2022-03-04 17:37:06 +0100

    initial commit

--- src/ZeroInflation.jl
+++ src/ZeroInflation.jl
@@ -1,5 +1,49 @@
 module ZeroInflation
 
-# Write your package code here.
+using Distributions
+using LogExpFunctions
+using Random
+
+export ZeroInflated
+export zeroinflated
+
+struct ZeroInflated{D<:UnivariateDistribution,S<:ValueSupport,T<:Real} <: UnivariateDistribution{S}
+  original::D
+  θ::T
+  p::T
+  link::Function
+  function ZeroInflated(d::UnivariateDistribution, θ::T, link = identity) where {T<:Real}
+    p = link(θ)
+    @assert 0.0 <= p <= 1.0
+    new{typeof(d),Distributions.value_support(typeof(d)),typeof(p)}(d, θ, p, link)
+  end
+end
+
+Distributions.params(d::ZeroInflated) = tuple(Distributions.params(d.original)..., d.link(d.p))
+
+Distributions.minimum(d::ZeroInflated) = min(minimum(d.original), 0)
+Distributions.maximum(d::ZeroInflated) = max(maximum(d.original), 0)
+
+function Distributions.logpdf(d::ZeroInflated, x::Real)
+  p = d.p
+  if x == 0
+    loglik = [
+      logpdf(Bernoulli(p), 1),
+      logpdf(Bernoulli(p), 0) + logpdf(d.original, x)
+    ]
+    return logsumexp(loglik)
+  else
+    return logpdf(Bernoulli(p), 0) + logpdf(d.original, x)
+  end
+end
+
+Distributions.pdf(d::ZeroInflated, x::Real) = exp(logpdf(d, x))
+
+function Distributions.rand(rng::Random.AbstractRNG, d::ZeroInflated)
+  c = rand(rng, Bernoulli(d.p))
+  return c == 1 ? zero(eltype(d.original)) : rand(rng, d.original)
+end
+
+zeroinflated(d::UnivariateDistribution, p::T; link = identity) where {T<:Real} = ZeroInflated(d, p, link)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,27 @@
+using Distributions
 using ZeroInflation
 using Test
 
 @testset "ZeroInflation.jl" begin
-    # Write your tests here.
+    @testset "constructing ZeroInflated" begin end
+
+    @testset "accessing ZeroInflated" begin
+        d = zeroinflated(Poisson(2.0), 0.5)
+        @test params(d) == (2.0, 0.5)
+        @test minimum(d) == 0
+        @test maximum(d) == Inf
+
+        d = zeroinflated(Normal(), 0.1)
+        @test params(d) == (0.0, 1.0, 0.1)
+        @test minimum(d) == -Inf
+        @test maximum(d) == Inf
+    end
+
+    @testset "rand" begin
+        @test rand(zeroinflated(Normal(), 1.0)) == 0.0
+        @test rand(zeroinflated(Normal(), 0.0)) != 0.0
+
+        @test rand(zeroinflated(Normal(), 0.0)) isa AbstractFloat
+        @test rand(zeroinflated(Poisson(1.0), 0.0)) isa Integer
+    end
 end
