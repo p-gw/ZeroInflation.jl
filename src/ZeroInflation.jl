9e1ba6e2488d1f591317f5056330492d8cfbafc2
Philipp Gewessler <p.gewessler@gmx.at>
2022-03-07 22:54:00 +0100

    fix indenting

--- src/ZeroInflation.jl
+++ src/ZeroInflation.jl
@@ -9,11 +9,11 @@ using UnPack
 export ZeroInflated, zeroinflated
 
 struct ZeroInflated{D1<:UnivariateDistribution,D2<:UnivariateDistribution,S<:ValueSupport} <: UnivariateDistribution{S}
-  original::D1
-  mix::D2
-  function ZeroInflated(d::UnivariateDistribution, m::UnivariateDistribution)
-    new{typeof(d),typeof(m),Distributions.value_support(typeof(d))}(d, m)
-  end
+    original::D1
+    mix::D2
+    function ZeroInflated(d::UnivariateDistribution, m::UnivariateDistribution)
+        new{typeof(d),typeof(m),Distributions.value_support(typeof(d))}(d, m)
+    end
 end
 
 Distributions.params(d::ZeroInflated) = tuple(Distributions.params(d.original)..., Distributions.params(d.mix)...)
@@ -21,38 +21,38 @@ Distributions.minimum(d::ZeroInflated) = min(minimum(d.original), minimum(d.mix)
 Distributions.maximum(d::ZeroInflated) = max(maximum(d.original), minimum(d.mix))
 
 function Distributions.logpdf(d::ZeroInflated, x::Real)
-  @unpack original, mix = d
-  if x == 0
-    loglik = [
-      logpdf(mix, 1),
-      logpdf(mix, 0) + logpdf(original, x)
-    ]
-    return logsumexp(loglik)
-  else
-    return logpdf(mix, 0) + logpdf(original, x)
-  end
+    @unpack original, mix = d
+    if x == 0
+        loglik = [
+            logpdf(mix, 1),
+            logpdf(mix, 0) + logpdf(original, x)
+        ]
+        return logsumexp(loglik)
+    else
+        return logpdf(mix, 0) + logpdf(original, x)
+    end
 end
 
 function Distributions.logpdf(d::ZeroInflated, x::Vector{T}) where {T<:Real}
-  @unpack original, mix = d
-  N_zero = sum(x .== 0)
-  N_nonzero = length(x) - N_zero
+    @unpack original, mix = d
+    N_zero = sum(x .== 0)
+    N_nonzero = length(x) - N_zero
 
-  loglik = N_zero * logsumexp([
-    logpdf(mix, 1),
-    logpdf(mix, 0) + logpdf(original, 0)
-  ])
-  loglik += N_nonzero * logpdf(mix, 0)
-  loglik += sum(logpdf.(original, filter(!iszero, x)))
-  return loglik
+    loglik = N_zero * logsumexp([
+        logpdf(mix, 1),
+        logpdf(mix, 0) + logpdf(original, 0)
+    ])
+    loglik += N_nonzero * logpdf(mix, 0)
+    loglik += sum(logpdf.(original, filter(!iszero, x)))
+    return loglik
 end
 
 Distributions.pdf(d::ZeroInflated, x::Real) = exp(logpdf(d, x))
 
 function Distributions.rand(rng::Random.AbstractRNG, d::ZeroInflated)
-  @unpack original, mix = d
-  c = rand(rng, mix)
-  return c == 1 ? zero(eltype(original)) : rand(rng, original)
+    @unpack original, mix = d
+    c = rand(rng, mix)
+    return c == 1 ? zero(eltype(original)) : rand(rng, original)
 end
 
 zeroinflated(d::UnivariateDistribution, p::T) where {T<:Real} = ZeroInflated(d, Bernoulli(p))
